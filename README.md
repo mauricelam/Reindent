952ede99a46819744f9db806d37b3a6c97489771
Maurice Lam <me@mauricelam.com>
2013-07-14 12:44:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Paste and Indent (PAI)
+==========
+
+Paste and indent is a simple sublime text plugin to fix the behavior of the built in Paste and Indent feature. It indents new lines as well when pasting.
+
+Installation
+----------
+
+Download all the files in this repository and place it in your Sublime packages folder. 
+* On the Mac it is `~/Library/Application Support/Sublime Text 2/Packages/PAI`
+
+Usage
+----------
+
+The default keyboard shortcut is `Cmd+Shift+V` on Mac or `Ctrl-shift+V` on Windows and Linux. The command name is `paste_pai` so you can change the keyboard shortcut to anything you want. 
---
+++ Reindent.py
@@ -0,0 +1,46 @@
+import sublime
+import sublime_plugin
+
+settings = sublime.load_settings("Reindent.sublime-settings")
+
+
+class PasteAndReindent(sublime_plugin.TextCommand):
+
+    def clone_region_set(self, region_set):
+        clone = []
+        for region in region_set:
+            clone.append(region)
+        return clone
+
+    def run(self, edit):
+        region = self.view.line(self.view.sel()[0]) if len(self.view.sel()) == 1 else None
+        command = 'use_paste_and_indent' if settings.get('use_paste_and_indent') else 'paste'
+        self.view.run_command(command)
+        if region is not None:
+            original_sel = self.clone_region_set(self.view.sel())
+            selections = self.view.sel()
+            for sel in selections:
+                self.view.sel().add(sel.cover(region))
+            self.view.run_command('reindent')
+
+            for sel in selections:
+                selections.subtract(sel)
+                selections.add(sublime.Region(sel.end(), sel.end()))
+
+
+class MoveDownAndReindent(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        self.view.run_command('expand_selection', {'to': 'line'})
+        self.view.run_command('move', {'by': 'characters', 'extend': True, 'forward': False})
+        self.view.run_command('swap_line_down')
+        self.view.run_command('reindent')
+
+
+class MoveUpAndReindent(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        self.view.run_command('expand_selection', {'to': 'line'})
+        self.view.run_command('move', {'by': 'characters', 'extend': True, 'forward': False})
+        self.view.run_command('swap_line_up')
+        self.view.run_command('reindent')
